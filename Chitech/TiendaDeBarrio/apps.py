2e159ad0f131f33b39e48dc5d3163e9c5bcee454
David Fonseca <47060767+dgfonseca@users.noreply.github.com>
2020-05-27 18:21:40 -0500

    Todo

---
+++ Chitech/TiendaDeBarrio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TiendadebarrioConfig(AppConfig):
+    name = 'TiendaDeBarrio'
---
+++ Chitech/TiendaDeBarrio/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+
+
+
